c96b744574bb0ad16f60190b73f6aa03229a6ab6
ricky <ricky.badhai9852@gmail.com>
2021-11-13 19:44:20 +0530

    deleted ex file

--- src/ex.js
+++
@@ -1,9 +0,0 @@
-
-            const csvFilePath='participants_85160701068.csv'
-            const csv=require('csvtojson')
-            console.log(csvFilePath);
-            csv()
-            .fromFile(csvFilePath)
-            .then((jsonObj)=>{
-                console.log(jsonObj);
-            })
